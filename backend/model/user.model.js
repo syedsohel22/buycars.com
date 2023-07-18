8b0e67271120c6973e443fc387a0574f78de5359
sohel <mr.sohelsyed@gmail.com>
2023-07-19 00:11:39 +0530

    modles of mongoose

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: { type: String, require: true },
-  email: { type: String, require: true },
-  password: { type: String, require: true },
-  isDealer: { type: Boolean, require: true },
-});
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    isDealer: { type: Boolean, require: true },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-
+const userModel = mongoose.model("user", userSchema);
+module.exports = userModel;
